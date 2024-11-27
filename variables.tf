0f01f66b424313b5774f2eace186caa0241a38f9
BohdanMatiuk <mbs.dvp@gmail.com>
2024-11-27 01:14:59 +0000

    anymessage

--- variables.tf
+++ variables.tf
@@ -5,3 +5,19 @@ variable region {
 variable vpc_cidr {
     type = string
 }
+
+variable subnet1_cidr {
+ type = string    
+
+}
+variable subnet2_cidr {
+ type = string    
+}
+
+variable subnet3_cidr {
+ type = string    
+}
+
+variable  map_public_ip_on_launch {
+    type = bool
+}
